58605d2e7b76a7f02a4dd108d0b890c891c24ae7
Carl <e12341573@student.tuwien.ac.at>
2025-05-06 17:49:10 +0200

    stats disabled

--- src/results.py
+++ src/results.py
@@ -20,30 +20,7 @@ def open_stats_page(root, var_manager):
 
     # Fetch and display statistics
     # TODO: Use read_csv_show_stats function to read the CSV files and display the statistics
-    stats = read_csv_show_stats("src/results/Type1")
-    """          Per km       Monthly        Yearly
-        count  80.000000     80.000000  8.000000e+01
-        mean    5.272748  26530.611497  3.183673e+05
-        std     7.011889  35285.268632  4.234232e+05
-        min     0.980000   4793.175194  5.751810e+04
-        25%     1.243853   6379.886722  7.655864e+04
-        50%     1.401982   6891.507995  8.269810e+04
-        75%     5.350160  26826.655169  3.219199e+05
-        max    17.469937  88852.906841  1.066235e+06
-    """
-
-    # Read the mean and std from the stats and display them in the text box
-    stats_text.insert(tk.END, "Statistics:\n")
-    stats_text.insert(tk.END, "Per km:\n")
-    stats_text.insert(tk.END, f"Mean: {stats['Per km']['mean']:.2f}\n")
-    stats_text.insert(tk.END, f"Std: {stats['Per km']['std']:.2f}\n")
-    stats_text.insert(tk.END, "Monthly:\n")
-    stats_text.insert(tk.END, f"Mean: {stats['Monthly']['mean']:.2f}\n")
-    stats_text.insert(tk.END, f"Std: {stats['Monthly']['std']:.2f}\n")
-    stats_text.insert(tk.END, "Yearly:\n")
-    stats_text.insert(tk.END, f"Mean: {stats['Yearly']['mean']:.2f}\n")
-    stats_text.insert(tk.END, f"Std: {stats['Yearly']['std']:.2f}\n")
-
+    stats = read_csv_show_stats("src/results/Type4")
 
     # Create a button to go back to the main menu
     back_button = tk.Button(stats_frame, text="Back to Main Menu", command=lambda: root.destroy())
@@ -66,6 +43,7 @@ def read_csv_show_stats(folder_path):
 
     # Concatenate all DataFrames into one
     all_data = pd.concat(dataframes, ignore_index=True)
+    print(all_data.head())  # Display the first few rows of the concatenated DataFrame
 
     # Display some key statistics
     stats = all_data.describe()
